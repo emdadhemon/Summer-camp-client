fdad871f3c05b7b3b7d481980b9471cff2c53d4c
emdadhemon <emdadhossainemon@gmail.com>
2023-06-13 12:17:08 +0600

    slider done

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import AwesomeSlider from 'react-awesome-slider';
+import 'react-awesome-slider/dist/styles.css';
+import 'react-awesome-slider/dist/custom-animations/cube-animation.css';
+import img1 from "../../../assets/1.jpg"
+import img2 from "../../../assets/2.jpg"
+import img3 from "../../../assets/3.jpg"
+
 
 const Banner = () => {
     return (
         <div>
-            
+            <AwesomeSlider animation="cubeAnimation">
+                <div data-src={img1}/>
+                <div data-src={img2}/>
+                <div data-src={img3}/>
+            </AwesomeSlider>
         </div>
     );
 };
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Navbar from '../../Shared/Navbar/Navbar';
 import { Outlet } from 'react-router-dom';
+import Banner from '../Banner/Banner';
 
 const Home = () => {
     return (
         <div>
-            
+            <Banner></Banner>
         </div>
     );
 };
